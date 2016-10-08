6f4ca2e574481159a8c4cdb66b9895f1ed3b08b0
Tran Bao <tran.bao51093@gmail.com>
2016-10-08 10:38:18 +0300

    add some filters & table overflow-wrap css

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,5 @@
 import Task from 'data.task';
-import { compose, map, prop, pick, over, lensProp, head, keys } from 'ramda';
+import { compose, map, prop, pick, over, lensProp, head, keys, split } from 'ramda';
 
 // PURE LIBS
 
@@ -16,14 +16,18 @@ const getRequest = url => new Task((rej, res) => {
 // url :: String -> Url
 const makeUrl = language => `https://api.github.com/search/repositories?q=language:${language}&sort=stars&order=desc`;
 
+const makeDate = compose(head, split('T'));
+
+const dateFilter = over(lensProp('created_at'), makeDate);
+
 const ownerFilter = over(lensProp('owner'), prop('login'));
 
 const makeItems = compose(
-  map(compose(ownerFilter, pick(['name', 'owner', 'description', 'created_at', 'stargazers_count']))), prop('items')
+  map(compose(dateFilter, ownerFilter, pick(['name', 'owner', 'description', 'created_at', 'stargazers_count']))), prop('items')
 );
 
 // getItemProperties :: [Item] => [ItemProperties]
-const getItemProperties = compose(keys, head);
+const getItemProperties = compose(map(k => ((k === 'stargazers_count') ? 'stars' : k)), keys, head);
 
 const ghTrendSearch = compose(map(makeItems), getRequest, makeUrl);
 
