94808e4a12c1a24281f553ede6ec145054637a1d
Tran Bao <tran.bao51093@gmail.com>
2016-10-09 09:53:43 +0300

    init mocha test

---
+++ src/components/ReposTable.test.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('Repos Table test', () => {
+  it('should pass', () => {
+    expect(true).toEqual(true);
+  });
+});
---
+++ test/testSetup.js
@@ -0,0 +1,23 @@
+// transpile ES6 to ES5 before tests run.
+require('babel-register')();
+
+// Configure JSDOM and set global variables
+// to simulate a browser environment for tests.
+var jsdom = require('jsdom').jsdom;
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js',
+};
+
+documentRef = document;
